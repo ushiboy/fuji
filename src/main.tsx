64120b400c03c0cd48dfec7041e3cd01d6371960
ushiboy <ushiboy.dev@gmail.com>
2023-11-12 11:17:38 +0900

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import TagManager from "react-gtm-module";
 import { App } from "./presentations";
+
+TagManager.initialize({
+  gtmId: import.meta.env.VITE_GTM_ID,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/presentations/App.tsx
+++ src/presentations/App.tsx
@@ -1,11 +1,27 @@
-import { HashRouter } from "react-router-dom";
-
+import { useEffect, PropsWithChildren } from "react";
+import { HashRouter, useLocation } from "react-router-dom";
+import TagManager from "react-gtm-module";
 import { AppRoutes } from "./AppRoutes";
 
 export function App() {
   return (
     <HashRouter>
-      <AppRoutes />
+      <TagManagerWrapper>
+        <AppRoutes />
+      </TagManagerWrapper>
     </HashRouter>
   );
+}
+
+function TagManagerWrapper({ children }: PropsWithChildren) {
+  const location = useLocation();
+  useEffect(() => {
+    TagManager.dataLayer({
+      dataLayer: {
+        event: "page_view",
+        pagePath: location.pathname,
+      },
+    });
+  }, [location.pathname]);
+  return children;
 }
